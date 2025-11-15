c1010cea519eec35b61205d56d37c124944be844
sougabrielxd <gabriellucasafb@gmail.com>
2025-11-15 15:05:44 -0300

    Alterações para landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
+  {/* <Route path="/about" component={About} />
       <Route path="/projects" component={Projects} />
       <Route path="/contact" component={Contact} />
       <Route path="/404" component={NotFound} />
